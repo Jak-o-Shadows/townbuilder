51434d2e09f9b3d75c9d4ab7a75dc931581d12ed
Jak-o-Shadows <jak-o-shadows@users.noreply.github.com>
2026-07-04 21:54:04 +0930

    Start setting up docs - see https://github.com/patdhlk/spec-driven-development/tree/main

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,12 +13,15 @@ author = 'Jak-o-Shadows'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx_needs",
+    "sphinx_codelinks",
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
+needs_from_toml = "ubproject.toml"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
