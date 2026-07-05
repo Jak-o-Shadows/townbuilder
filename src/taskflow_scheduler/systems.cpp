a604c32a9f6774e4e846057f3cbd259e15c6d69b
Jak-o-Shadows <jak-o-shadows@users.noreply.github.com>
2026-07-05 11:38:03 +0930

    add listging of inputs & outputs

--- src/taskflow_scheduler/systems.cpp
+++ src/taskflow_scheduler/systems.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <memory>
 #include <sstream>
+#include <format>
 
 namespace TaskflowScheduler {
 
@@ -32,7 +33,6 @@ systems::systems(flecs::world& ecs) {
         .without(flecs::Disabled).up(flecs::ChildOf)
         .build();
 
-    // System to list all other systems
     ecs.system("SystemLister")
         .run([system_query](flecs::iter& it) {
             ZoneScopedN("SystemLister");
@@ -44,6 +44,37 @@ systems::systems(flecs::world& ecs) {
             });
             msg << "END FRAME";
             systemsLogger->debug("{}", msg.str());
+        });
+
+    ecs.system("SystemInOutLister")
+        .run([system_query, &ecs](flecs::iter& it) {
+            ZoneScopedN("SystemInOutLister");
+            systemsLogger->debug("Listing all systems in & outs:");
+
+            system_query.each([&ecs](flecs::entity system_entity) {
+                ecs_entity_t id = system_entity.id();
+                const ecs_system_t *s = ecs_system_get(ecs.c_ptr(), id);
+                if (!s || !s->query) {
+                    systemsLogger->debug("{} : <no system data>", system_entity.path().c_str());
+                    return;
+                }
+                std::ostringstream msg;
+                msg << std::format("System: {} : ", system_entity.path().c_str());
+                for (int t = 0; t < s->query->term_count; ++t) {
+                    const ecs_term_t *term = &s->query->terms[t];
+                    // term->id is the component id; term->inout is the access kind
+                    const char *comp = ecs_get_name(ecs.c_ptr(), term->id);
+                    const char *access = "unknown";
+                    switch (term->inout) {
+                        case EcsIn: access = "in"; break;
+                        case EcsOut: access = "out"; break;
+                        case EcsInOut: access = "inout"; break;
+                        default: access = "other"; break;
+                    }
+                    msg << std::format("{}({}) ;", (comp ? comp : "<comp>"), access);
+                }
+                systemsLogger->debug("{} END", msg.str());
+            });
         });
 
 }
